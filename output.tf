c567212f4657f6b8771a26dd90030f420144af10
Juan Arillo <juan.arillo@apppadel.com>
2025-02-17 14:36:36 +0100

    fixed website_endpoint deprecation from aws_s3_bucket to aws_s3_bucket_website_configuration

--- output.tf
+++ output.tf
@@ -1,11 +1,11 @@
 output "static_website_endpoint" {
   value = <<-EOT
-  
+
   Access
   ------
   To access to the static website, please visit the following URL:
 
-  http://${aws_s3_bucket.juanarillo-aws-practice.website_endpoint}
+  http://${aws_s3_bucket_website_configuration.juanarillo-aws-practice.website_endpoint}
 
     EOT
 }
